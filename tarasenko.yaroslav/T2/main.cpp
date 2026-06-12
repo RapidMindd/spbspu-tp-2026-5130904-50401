5583b9c9b92e7681a67b75e6c99d5476c3879638
RapidMindd <YaroslavT007@yandex.ru>
2026-06-12 22:45:44 +0300

    less operator

--- tarasenko.yaroslav/T2/main.cpp
+++ tarasenko.yaroslav/T2/main.cpp
@@ -10,6 +10,19 @@ namespace tarasenko
     std::string key3;
   };
 
+  bool operator<(const DataStruct& lhs, const DataStruct& rhs)
+  {
+    if (lhs.key1 != rhs.key1)
+    {
+      return lhs.key1 < rhs.key1;
+    }
+    if (lhs.key2 != rhs.key2)
+    {
+      return lhs.key2 < rhs.key2;
+    }
+    return lhs.key3.size() < rhs.key3.size();
+  }
+
   struct del
   {
     std::string s;
