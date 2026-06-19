487d83ba581d09913cb5ddd42fddee3537960c8f
RapidMindd <YaroslavT007@yandex.ru>
2026-06-19 12:04:55 +0300

    calculate triangle and polygon area

--- tarasenko.yaroslav/T3/main.cpp
+++ tarasenko.yaroslav/T3/main.cpp
@@ -6,6 +6,10 @@
 #include <functional>
 #include <iterator>
 #include <fstream>
+#include <unordered_map>
+#include <numeric>
+#include <limits>
+#include <iomanip>
 
 namespace tarasenko
 {
@@ -31,6 +35,11 @@ namespace tarasenko
   struct Polygon
   {
     std::vector< Point > points;
+  };
+
+  struct Triangle
+  {
+    Point points[3];
   };
 
   using Polygons = std::vector< Polygon >;
@@ -115,6 +124,53 @@ namespace tarasenko
     std::copy(polygon.points.begin(), polygon.points.end(), oit(out, " "));
     return out;
   }
+
+  struct TriangleGenerator
+  {
+    const Polygon& polygon;
+    int index;
+
+    TriangleGenerator(const Polygon& polygon):
+      polygon(polygon),
+      index(0)
+    {}
+
+    Triangle operator()()
+    {
+      Triangle triangle;
+      triangle.points[0] = polygon.points[0];
+      triangle.points[1] = polygon.points[index + 1];
+      triangle.points[2] = polygon.points[index + 2];
+      ++index;
+      return triangle;
+    }
+  };
+
+  double getTriangleArea(const Triangle& triangle)
+  {
+    Point a = triangle.points[0];
+    Point b = triangle.points[1];
+    Point c = triangle.points[2];
+    return 0.5 * std::abs(a.x * (b.y - c.y) + b.x * (c.y - a.y) + c.x * (a.y - b.y));
+  }
+
+  double getPolygonArea(const Polygon& polygon)
+  {
+    std::vector< Triangle > triangles;
+    size_t triangle_count = polygon.points.size() - 2;
+    std::generate_n(std::back_inserter(triangles), triangle_count, TriangleGenerator(polygon));
+    std::vector< double > areas;
+    std::transform(triangles.begin(), triangles.end(), std::back_inserter(areas), getTriangleArea);
+    return std::accumulate(areas.begin(), areas.end(), 0.0);
+  }
+
+  void calculateAreas(std::istream&, std::ostream& out, const Polygons& polygons)
+  {
+    IOguard guard(out);
+    std::vector< double > areas;
+    std::transform(polygons.begin(), polygons.end(), std::back_inserter(areas), getPolygonArea);
+    out << std::fixed << std::setprecision(1) << std::accumulate(areas.begin(), areas.end(), 0.0) << '\n';
+  }
 }
 
 int main(int argc, char** argv)
@@ -130,9 +186,29 @@ int main(int argc, char** argv)
   using iit = std::istream_iterator< Polygon >;
   std::ifstream in(argv[1]);
   std::copy(iit(in), iit{}, std::back_inserter(polygons));
-  using oit = std::ostream_iterator< Polygon >;
-  std::copy(polygons.begin(), polygons.end(), oit(std::cout, "\n"));
-  return 0;
+
+  using cmd_t = std::function< void(std::istream&, std::ostream&, Polygons&) >;
+  using const_cmd_t = std::function< void(std::istream&, std::ostream&, const Polygons&) >;
+  std::unordered_map< std::string, cmd_t > cmds;
+
+  std::unordered_map< std::string, const_cmd_t > const_cmds;
+  cmds["AREA"] = calculateAreas;
+
+  std::string cmd;
+  while (std::cin >> cmd)
+  {
+    try
+    {
+      cmds.at(cmd)(std::cin, std::cout, polygons);
+    }
+    catch (const std::exception&)
+    {
+      std::cout << "<INVALID COMMAND>\n";
+      auto toignore = std::numeric_limits< std::streamsize >::max();
+      std::cin.clear();
+      std::cin.ignore(toignore, '\n');
+    }
+  }
 }
 
 tarasenko::IOguard::IOguard(std::basic_ios< char >& s):
